d5bcde650b7d51dcf723c46e0665031328ed902e
seethlakshmi3-1999 <105357112+seethlakshmi3-1999@users.noreply.github.com>
2022-05-17 13:33:02 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 st.title('My Parents New Healthy Diner')
 st.header('Breakfast Menu')
-st.write('Omega and 3 omlets')
+st.write('Omega & 3 omlets')
 st.write('Spinach,Strawberry')
 st.write('hard Boiled')
