956b968724c569050304c420d9d1c5bea86052e2
seethlakshmi3-1999 <105357112+seethlakshmi3-1999@users.noreply.github.com>
2022-05-17 13:34:07 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
-import streamlit as st
-st.title('My Parents New Healthy Diner')
-st.header('Breakfast Menu')
-st.write('Omega & 3 omlets')
-st.write('Spinach,Strawberry')
-st.write('hard Boiled')
+import streamlit
+streamlit.title('My Parents New Healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
